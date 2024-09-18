3f8bc2830837a1214ecfbbcc657b9edd5e914f82
Angie Granados <angiekatherine.granadosleal@emeal.nttdata.com>
2024-09-18 22:09:29 +0000

    Haciendo nuevamente el código

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,9 +5,10 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "sap/btp/helloworldui5/model/models"
+        "sap/btp/helloworldui5/model/models",
+        "sap/base/Log"
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, Log) {
         "use strict";
 
         return UIComponent.extend("sap.btp.helloworldui5.Component", {
@@ -24,6 +25,12 @@ sap.ui.define([
                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
 
+                Log.info("Hello-World Information Log");
+                Log.debug("Hello-World Debug Log");
+                Log.warning("Hello-World Warning Log");
+                Log.error("Hello-World Error Log");
+                Log.fatal("Hello-World Fatal Log");
+                
                 // enable routing
                 this.getRouter().initialize();
 
